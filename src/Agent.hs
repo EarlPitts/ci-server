11999f52755cb7f9d86b5b8b71cb5ab4f5f6c5ec
Bendeguz Seres <grafitpor@gmail.com>
2023-10-13 08:36:42 +0200

    Implement main functionality in agent

--- src/Agent.hs
+++ src/Agent.hs
@@ -1,10 +1,17 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Agent where
 
 import Codec.Serialise qualified as Serialise
 import Core
+import Network.HTTP.Simple qualified as HTTP
 import RIO
+import Runner qualified
 
 data Cmd
   = StartBuild BuildNumber Pipeline
@@ -14,3 +21,33 @@ data Msg
   = LogCollected BuildNumber Log
   | BuildUpdated BuildNumber Build
   deriving (Eq, Show, Generic, Serialise.Serialise)
+
+data Config = Config
+  { endpoint :: String
+  }
+
+runCommand :: Runner.Service -> Cmd -> IO ()
+runCommand runner = \case
+  StartBuild number pipeline -> do
+    let hooks =
+          Runner.Hooks
+            { logCollected = traceShowIO -- TODO
+            }
+
+    build <- runner.prepareBuild pipeline
+    void $ runner.runBuild hooks build
+
+run :: Config -> Runner.Service -> IO ()
+run config runner = forever do
+  endpoint <- HTTP.parseRequest config.endpoint
+  let req =
+        endpoint
+          & HTTP.setRequestMethod "POST"
+          & HTTP.setRequestPath "/agent/pull"
+
+  res <- HTTP.httpLBS req
+  let cmd = Serialise.deserialise (HTTP.getResponseBody res) :: Maybe Cmd
+
+  traverse_ (runCommand runner) cmd
+
+  threadDelay (1 * 1000 * 1000)
--- src/Server.hs
+++ src/Server.hs
@@ -1,15 +1,15 @@
-{-# LANGUAGE OverloadedRecordDot #-}
-{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Server where
 
+import Codec.Serialise qualified as Serialise
 import Core
 import JobHandler qualified
 import RIO
 import Web.Scotty qualified as Scotty
-import qualified Codec.Serialise as Serialise
 
 data Config = Config
   { port :: Int
@@ -18,7 +18,6 @@ data Config = Config
 run :: Config -> JobHandler.Service -> IO ()
 run config handler =
   Scotty.scotty config.port do
-
     Scotty.post "/agent/pull" do
       cmd <- Scotty.liftAndCatchIO do
         handler.dispatchCmd
